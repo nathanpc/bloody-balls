7c02354bf759d5c16b0c05965b3394ce85e25b6c
Nathan Campos <nathan@innoveworkshop.com>
2022-05-17 23:01:28 +0100

    Changed line endings

--- Assets/Scripts/Managers/NotificationManager.cs
+++ Assets/Scripts/Managers/NotificationManager.cs
@@ -4,92 +4,94 @@ using UnityEngine;
 using UnityEngine.UI;
 using BloodyBalls.Utilities;
 
-/// <summary>
-/// Manages the notification the player will get while playing the game.
-/// </summary>
-public class NotificationManager : MonoBehaviour {
-	[Header("Components")]
-	[SerializeField] private RectTransform notificationBox;
-	[SerializeField] private Text textBox;
-
-	[Header("Animation")]
-	[SerializeField] private int _displayDuration = 10;
-	[SerializeField] protected int animationDuration = 1;
-
-	private Vector2 hiddenPosition;
-	private Vector2 shownPosition;
-
-	// Start is called before the first frame update
-	void Start() {
-		hiddenPosition = notificationBox.position;
-		shownPosition = notificationBox.position;
-
-		Rect rect = notificationBox.rect;
-		shownPosition.y -= rect.height;
-	}
-
+namespace BloodyBalls.Managers {
 	/// <summary>
-	/// Sets the text in the notification box.
+	/// Manages the notification the player will get while playing the game.
 	/// </summary>
-	/// <param name="text">Text of the notification.</param>
-	public void SetText(string text) {
-		textBox.text = text;
-	}
+	public class NotificationManager : MonoBehaviour {
+		[Header("Components")]
+		[SerializeField] private RectTransform notificationBox;
+		[SerializeField] private Text textBox;
 
-	/// <summary>
-	/// Gets the text used in the notification box.
-	/// </summary>
-	/// <returns>Notification text.</returns>
-	public string GetText() {
-		return textBox.text;
-	}
+		[Header("Animation")]
+		[SerializeField] private int _displayDuration = 10;
+		[SerializeField] protected int animationDuration = 1;
 
-	/// <summary>
-	/// Hides the notification box.
-	/// </summary>
-	public void Hide() {
-		Tween.Move(notificationBox, shownPosition, hiddenPosition, animationDuration);
-	}
+		private Vector2 hiddenPosition;
+		private Vector2 shownPosition;
 
-	/// <summary>
-	/// Hides the notification box.
-	/// </summary>
-	/// <param name="animationDuration">Duration of the animation in seconds.</param>
-	public void Hide(int animationDuration) {
-		Tween.Move(notificationBox, shownPosition, hiddenPosition, this.animationDuration,
-			animationDuration);
-	}
+		// Start is called before the first frame update
+		void Start() {
+			hiddenPosition = notificationBox.position;
+			shownPosition = notificationBox.position;
 
-	/// <summary>
-	/// Shows the notification box.
-	/// </summary>
-	public void Show() {
-		Tween.Move(notificationBox, hiddenPosition, shownPosition, animationDuration);
-	}
+			Rect rect = notificationBox.rect;
+			shownPosition.y -= rect.height;
+		}
 
-	/// <summary>
-	/// Shows the notification box.
-	/// </summary>
-	/// <param name="shownTime">Time for the notification to be displayed for in seconds.</param>
-	public void Show(int shownTime) {
-		Tween.Move(notificationBox, hiddenPosition, shownPosition, animationDuration);
-		Hide(shownTime);
-	}
+		/// <summary>
+		/// Sets the text in the notification box.
+		/// </summary>
+		/// <param name="text">Text of the notification.</param>
+		public void SetText(string text) {
+			textBox.text = text;
+		}
 
-	/// <summary>
-	/// Shows the notification box with a text for a pre-defined time, then hides it back again.
-	/// </summary>
-	/// <param name="text">Text to be shown in the notification.</param>
-	public void Notify(string text) {
-		SetText(text);
-		Show(Duration);
-	}
+		/// <summary>
+		/// Gets the text used in the notification box.
+		/// </summary>
+		/// <returns>Notification text.</returns>
+		public string GetText() {
+			return textBox.text;
+		}
 
-	/// <summary>
-	/// Duration to show the notification for.
-	/// </summary>
-	public int Duration {
-		get { return _displayDuration; }
-		set { _displayDuration = value; }
+		/// <summary>
+		/// Hides the notification box.
+		/// </summary>
+		public void Hide() {
+			Tween.Move(notificationBox, shownPosition, hiddenPosition, animationDuration);
+		}
+
+		/// <summary>
+		/// Hides the notification box.
+		/// </summary>
+		/// <param name="animationDuration">Duration of the animation in seconds.</param>
+		public void Hide(int animationDuration) {
+			Tween.Move(notificationBox, shownPosition, hiddenPosition, this.animationDuration,
+				animationDuration);
+		}
+
+		/// <summary>
+		/// Shows the notification box.
+		/// </summary>
+		public void Show() {
+			Tween.Move(notificationBox, hiddenPosition, shownPosition, animationDuration);
+		}
+
+		/// <summary>
+		/// Shows the notification box.
+		/// </summary>
+		/// <param name="shownTime">Time for the notification to be displayed for in seconds.</param>
+		public void Show(int shownTime) {
+			Tween.Move(notificationBox, hiddenPosition, shownPosition, animationDuration);
+			Hide(shownTime);
+		}
+
+		/// <summary>
+		/// Shows the notification box with a text for a pre-defined time, then hides it back again.
+		/// </summary>
+		/// <param name="text">Text to be shown in the notification.</param>
+		public void Notify(string text) {
+			SetText(text);
+			Show(Duration);
+		}
+
+		/// <summary>
+		/// Duration to show the notification for.
+		/// </summary>
+		public int Duration {
+			get { return _displayDuration; }
+			set { _displayDuration = value; }
+		}
 	}
 }
