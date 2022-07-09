db397bd8d23d435f576cd04ed2d34eb04c56abdd
Nathan Campos <nathan@innoveworkshop.com>
2022-07-09 20:18:56 +0100

    Updating the coins counter when using power ups

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -10,12 +10,14 @@ namespace BloodyBalls.PowerUps {
 	/// </summary>
 	public abstract class PowerUp : MonoBehaviour {
 		[SerializeField] protected int points = 1;
+		[SerializeField] protected UIManager uiManager;
 
 		/// <summary>
 		/// Activates the power-up.
 		/// </summary>
 		virtual public void Activate() {
 			Utils.RemoveCoins(points);
+			uiManager.SetHUDCoins(Utils.GetCoins());
 		}
 	}
 }
